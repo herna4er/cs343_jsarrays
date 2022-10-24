72e014b2584a447da895ab55a1fc739e5c28c1ab
Eric Hernandez Diaz <herna4er@dukes.jmu.edu>
2022-10-24 16:00:21 -0400

    Add files via upload

--- scripts/arrays.js
+++ scripts/arrays.js
@@ -1,6 +1,87 @@
 console.log(data);
 // 1. instead of creating the cards manually, we should use array functions to convert the data into cards
 
+const courseToCard = ({
+  prefix,
+  number,
+  title,
+  url,
+  desc,
+  prereqs,
+  credits,
+}) => {
+  const prereqLinks = prereqs
+    .map((prereq) => `<a href="#" class="card-link">${prereq}</a>`)
+    .join();
+  const courseTemplate = `<div class="col">
+            <div class="card" style="width: 18rem;">
+              <h3 class="card-header"><a href="${url}">${title}</a></h3>
+              <div class="card-body">
+                <h5 class="card-title">${prefix} ${number}</h5>
+                <p class="card-text">${desc}</p>
+                ${prereqLinks}
+                <div class="card-footer text-muted">
+                  ${credits}
+                </div>
+              </div>
+            </div>
+          </div>`;
+  return courseTemplate;
+};
+const resultsContainer = document.querySelector("#filtered-results");
+const courseCards = data.items.map(courseToCard);
+resultsContainer.innerHTML = courseCards.join("");
+// courseCards.forEach((c) => document.write(c));
+
+// console.log(courseCards);
+// document.write(courseCards.join(''))
+
 // 2. maybe we only show those that match the search query?
+//
+
+const filterCourseCard = (markup, query) => {
+  console.log(markup, query);
+  return markup.toLowerCase().includes(query.toLowerCase());
+};
+
+const searchButton = document.getElementById("search-btn");
+searchButton.addEventListener("click", (ev) => {
+  console.log(ev);
+  ev.preventDefault();
+  // ev.stopPropagation();
+  console.log("query text");
+  const searchField = document.querySelector('input[name="query-text"]');
+  const queryText = searchField.value;
+  console.log(queryText);
+  const filteredCourseCards = courseCards.filter((card) =>
+    filterCourseCard(card, queryText)
+  );
+  console.log('filteredCourseCards', filteredCourseCards);
+  resultsContainer.innerHTML = filteredCourseCards.join("");
+
+  const count = document.querySelectorAll(".card")
+  const class_count = document.getElementById("count");
+  class_count.innerText = `${count.length}`;
+
+  const filteredCourses = data.items.filter((card) => resultsContainer.innerHTML.toLowerCase().includes(card.title.toLowerCase()))
+  console.log(filteredCourses)
+
+  let sum = 0;
+  let total = filteredCourses.reduce((prev,p) => prev + p.credits, sum)
+  const credit_hours = document.getElementById("credit-hours");
+  credit_hours.innerText = total;
+
+
+
+});
 
 // 3. we update the result count and related summary info as we filter
+
+const count = document.querySelectorAll(".card")
+const class_count = document.getElementById("count");
+class_count.innerText = `${count.length}`;
+
+let sum = 0;
+let total = data.items.reduce((prev,p) => prev + p.credits, sum)
+const credit_hours = document.getElementById("credit-hours");
+credit_hours.innerText = total;
--- scripts/data.js
+++ scripts/data.js
@@ -30,5 +30,23 @@ const data = {
       prereqs: [159],
       credits: 3,
     },
+    {
+      prefix: "CS",
+      number: 347,
+      title: "Full-stack Web Development",
+      url: "https://catalog.jmu.edu/preview_course_nopop.php?catoid=50&coid=257214",
+      desc: "This course covers design and development of World Wide Web applications. Students examine protocols, architectures, development languages and facilities, and issues such as accessibility, usability, security and reliability. Students work with a suite of development tools and languages to construct a client or server application that includes a GUI and a database.",
+      prereqs: [343],
+      credits: 3,
+    },
+    {
+      prefix: "CS",
+      number: 447,
+      title: "Interaction Design",
+      url: "https://catalog.jmu.edu/preview_course_nopop.php?catoid=50&coid=257217",
+      desc: "Study of and practice with processes, principles, tools, models and techniques for designing interactions between humans and digital products and systems. Topics include physiological and psychological factors affecting interaction design, interaction design processes, interaction models, styles and paradigms, design notations and representations, prototyping, and interaction design evaluation.",
+      prereqs: [343, 789],
+      credits: 3,
+    },
   ],
 };
